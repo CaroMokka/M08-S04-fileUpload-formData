30640e25ac8c8939aa4c627422783e67005ba623
carolina <ci.arayagonzalez@gmail.com>
2024-12-04 11:46:25 -0300

    Obtener, renombrar y guardar archivo de imagen con validaciones

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-import express from "express"
-import fileUpload from "express-fileupload"
+import express from "express";
+import fileUpload from "express-fileupload";
+import { dirname } from "path";
 import path from "path";
 import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
+const app = express();
+const port = 3000;
 
-const app = express()
-
-const port = 3000
-
-app.use(express.json())
-app.use(fileUpload({ createParentPath: true }))
-app.use("/public", express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(fileUpload({ createParentPath: true }));
+app.use("/public", express.static(`${__dirname}/public`));
 
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`)
-})
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+app.post("/registro", async (req, res) => {
+  if (req.files) {
 
-app.post("/registro", (req, res) => {
-    console.log(req.body)
-    console.log(req.files)
-    const archivo = req.files.foto
-    const filename = uuid()
-    const { ext } = path.parse(req.files.foto.name)
-    console.log({ext})
+    const filename = uuidv4();
+    const archivo = req.files.foto;
+    const { ext } = path.parse(req.files.foto.name);
 
-    if(archivo.mimetype != "image/jpg" ){
-
+    if (archivo.size > 5_000_000) {
+      return res.status(422).json({ mesagge: "El archivo excede los 5MB" });
     }
-    res.json({ message: "Registro exitoso." })
+    if (
+      archivo.mimetype != "image/png" &&
+      archivo.mimetype != "image/jpg" &&
+      archivo.mimetype != "image/jpeg"
+    ) {
+      return res
+        .status(422)
+        .json({ mesagge: "Acepta archivos png, jpg o jpeg." });
+    }
+    await archivo.mv(`${__dirname}/public/images/${filename}${ext}`)
+  }
 
-})
+  res.json({ message: "Registro exitoso" });
+});
 
 app.listen(port, () => {
-    console.log(`Aplicación en ejecución en el puerto ${port}`)
-})
+  console.log(`Aplicación en ejecución en el puerto ${port}`);
+});
